8afa5c3db634ae6104dce42916fa129c376c1cd9
PrettyJRicky <patrickjayestrada@yahoo.com>
2022-06-26 22:02:51 +0200

    Builder Pattern (Demo)

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,10 +1,17 @@
 package at.ac.fhcampuswien;
 
+import at.ac.fhcampuswien.models.Article;
 import at.ac.fhcampuswien.ui.Menu;
 
 public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.start();
+
+       /* Article testArticle = new Article.Builder("John Green", "Papertowns")
+                .description("Great book for teenagers.")
+                .build();
+
+        System.out.println(testArticle); */
     }
 }
