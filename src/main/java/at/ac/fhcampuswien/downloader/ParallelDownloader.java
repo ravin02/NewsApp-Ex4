6b0b71883b2593efbbe96120661497b15327262e
ravin02 <ravin.de.silva@stud.fh-campuswien.ac.at>
2022-06-26 23:28:32 +0200

    Multithreading (from Pascal) -> issues with push

--- src/main/java/at/ac/fhcampuswien/downloader/ParallelDownloader.java
+++ src/main/java/at/ac/fhcampuswien/downloader/ParallelDownloader.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-// Class is needed for exercise 4 - ignore for exercise 3 solution
+//Class is needed for exercise 4 - ignore for exercise 3 solution
 public class ParallelDownloader extends Downloader{
 
     // returns number of downloaded article urls
