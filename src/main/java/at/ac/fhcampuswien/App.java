69c61a5f11089a1c1d307c6cfe915d12347d0630
pascalito1020 <pascal_lotter@yahoo.com>
2022-07-12 09:31:00 +0200

    ed2edde

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -8,10 +8,11 @@ public class App {
         Menu menu = new Menu();
         menu.start();
 
-       /* Article testArticle = new Article.Builder("John Green", "Papertowns")
+     /*  Article testArticle = new Article.Builder("John Green", "Papertowns")
                 .description("Great book for teenagers.")
                 .build();
 
-        System.out.println(testArticle); */
+        System.out.println(testArticle);
+    }*/
     }
 }
--- src/test/java/at/ac/fhcampuswien/controllers/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/controllers/AppControllerTest.java
@@ -1,4 +1,41 @@
 package at.ac.fhcampuswien.controllers;
 
+import at.ac.fhcampuswien.models.Article;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppControllerTest {
+    private static AppController controller;
+
+
+    @BeforeAll
+    static void init(){
+
+    }
+
+    @BeforeEach
+    void setup(){
+        controller = AppController.getInstance();
+    }
+
+    @Test
+    @DisplayName("getAllNewsBitcoin Test 1")
+    public void getAllNewsBitcoin_scenario1(){
+
+        List<Article> ac = new ArrayList<>();
+        int i = controller.getAllNewsBitcoin().size();
+        int i2 = 100;
+
+        assertEquals(i, i2);
+
+    }
+
+
+
 }
